eb0541759621b2e6b0f86bf9c7a51ad55f7f0dfd
Annadurai Sevugaperumal <30621970+annadurai24@users.noreply.github.com>
2023-01-09 16:33:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,22 +24,24 @@ app.get("/employee", (req, res) => {
                  {
                     name : "Pothiraj S",
                     status: "Present",
+                    date : "09-jan-23", 
                     inTime : "09:32:02",
                     outTime : "06:23:11"
                 },
                  {
                     name : "Chittibabu S",
                     status: "Absent",
+                     date : "09-jan-23",
                     inTime : "",
                     outTime : ""
                 },
                  {
                     name : "Ganesan S",
                     status: "Present",
+                     date : "09-jan-23",
                     inTime : "09:25:00",
                     outTime : "07:01:23"
                 }
-        
         ]})
 })
 
